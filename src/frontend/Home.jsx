03d206e61779a6559db06a5b7c543f220b11499e
Romina Ghaderi <romina.ghaderi@outlook.com>
2022-04-21 17:14:54 +0200

    adding routes

---
+++ src/frontend/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Home() {
+  return (
+    <div>      
+            <div className="Links">
+
+            <button>
+                <Link to="/login" className="link">
+                Logga in på ett befintligt konto
+                </Link>
+            </button>
+
+            </div>
+            <div>
+
+            <button>
+                <Link to="/signup" className="link">
+                Registrera ett nytt konto
+                </Link>
+            </button>
+    </div>
+</div>
+  )
+}
+
+export default Home
