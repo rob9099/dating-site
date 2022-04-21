fa5a9b052b6e61ce5e3ca421c3091c2b4b9daa04
Romina Ghaderi <romina.ghaderi@outlook.com>
2022-04-21 17:14:45 +0200

    adding routes in frontend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import './css/styles.css';
+import Login from './frontend/Login'
+import Signup from './frontend/Signup'
+import Home from './frontend/Home'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
+<Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+        </Routes>
+      </div>
+
+    </Router>
       
     </div>
   );
