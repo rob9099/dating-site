6ae26796e0a477389e3137805958146e28d7490d
Romina Ghaderi <romina.ghaderi@outlook.com>
2022-04-26 18:04:19 +0200

    creating header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import '../css/styles.css'
+import React from 'react'
+import img from '../img/logo.gif'
+
+function Header() {
+  return (
+    <div className='Header'>
+
+        <img src={img} alt="img"/>
+
+    </div>
+  )
+}
+
+export default Header
